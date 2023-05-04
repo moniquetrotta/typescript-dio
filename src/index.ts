964161daa25211395f580ba5c68d4c250914d7e9
moniquetrotta <monique.trotta@gmail.com>
2023-05-04 15:14:19 -0300

    Class Decorator

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 //decorators
-function ExibirNome(target: any){
+/*function ExibirNome(target: any){
   console.log(target);
 }
 @ExibirNome
 class Funcionario {}
 
 @ExibirNome
-class Quincas{}
+class Quincas{}*/
+
+function apiVersion (version: string){
+  return (target: any) => {
+    Object.assign(target.prototype, {__version: version, __name: "niq"})
+  }
+}
+
+@apiVersion("1.10")
+class Api {}
+
+const api = new Api();
+console.log(api.__version);
+console.log(api.__name);
